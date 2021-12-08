a45fad0388ca180c9b6dd901432ac75b0e5b7ec5
Igor <igorcbraz1@gmail.com>
2021-12-07 23:52:03 -0300

    Add test data

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,22 @@ createServer({
   seeds(sever) {
     sever.db.loadData({
       transactions: [
-        
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
       ],
     })
   },
