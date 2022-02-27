3dd92fd51a2cf4c269920b6285beb90e53870668
Igor <igorcbraz1@gmail.com>
2022-02-27 15:20:42 -0300

    Change index to resolve header bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { App } from './App';
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom';
  
-// Was type any because this libary is made for js not ts
-const rootElement: any = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
